818ecceaa2dc62f5ceca79210713c6d2de44d9b4
Alexander Campbell <alexanderhcampbell@gmail.com>
2017-10-14 10:41:43 -0500

    whiteboarder.js: start mocking out handlers

--- whiteboarder-web/whiteboarder.js
+++ whiteboarder-web/whiteboarder.js
@@ -1,7 +1,13 @@
 
+var routePrefix = "/api/wb"
+
+function getSession(id) {
+        assert(typeof id == "string");
+        $.get(routePrefix + "/session/" + id);
+}
+
 $(function() {
         $("#wb-join-session").click(function() {
-                alert("what's up");
-                $.post("")
+                getSession();
         });
 });
