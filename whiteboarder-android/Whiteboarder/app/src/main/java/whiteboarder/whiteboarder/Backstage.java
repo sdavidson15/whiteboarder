13ccbd0887d6c20408cbdb2cf455fc0a5013324d
Alexander Campbell <alexanderhcampbell@gmail.com>
2017-10-24 18:08:27 -0500

    android: show message when "create session" is clicked

--- whiteboarder-android/Whiteboarder/app/src/main/java/whiteboarder/whiteboarder/Backstage.java
+++ whiteboarder-android/Whiteboarder/app/src/main/java/whiteboarder/whiteboarder/Backstage.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Backstage extends AppCompatActivity {
 
@@ -16,15 +17,16 @@ public class Backstage extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button b = (Button) findViewById(R.id.createSessionButton);
+        b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "wow", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
