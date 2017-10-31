07a89f5f9a49006fb6063bc71ca85e1b44aa17f4
Alexander Campbell <alexanderhcampbell@gmail.com>
2017-10-31 18:11:35 -0500

    web: try to post with $.ajax

--- whiteboarder-web/whiteboarder.js
+++ whiteboarder-web/whiteboarder.js
@@ -1,13 +1,19 @@
 
-var routePrefix = "/api/wb"
-
-function getSession(id) {
-        assert(typeof id == "string");
-        $.get(routePrefix + "/session/" + id);
-}
+var routePrefix = "/whiteboarder";
 
 $(function() {
-        $("#wb-join-session").click(function() {
-                getSession();
+    $("#wb-join-session").click(function() {
+        var url = routePrefix + "/session";
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: "",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function(data) {
+                alert(data);
+            }
         });
+    });
 });
+
