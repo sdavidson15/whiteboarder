1d68b9ff6623f06e4d968d5225f61d7d9273a307
Alexander Campbell <alexanderhcampbell@gmail.com>
2017-11-05 10:52:20 -0600

    web: integrate all code into index
    The app should be single-page.

--- whiteboarder-web/GenerateQR.js
+++ whiteboarder-web/GenerateQR.js
@@ -2,16 +2,6 @@ var routePrefix = "localhost:8080/whiteboarder/"
 
 function postSession() {
 
-        $.ajax('http://localhost:8080/whiteboarder/session', {
-                type: 'POST',
-                contentType: "application/json",
-                success: function (response) {
-                        // make cookie
-                        document.cookie = "wb_session_id=" + response + ";path=/";
-                        window.location.href = "GenerateQR.html"
-                },
-                dataType: 'text'
-        });
 }
 
 
--- whiteboarder-web/whiteboarder.js
+++ whiteboarder-web/whiteboarder.js
@@ -1,18 +1,42 @@
 
 var routePrefix = "/whiteboarder";
+var sessionID = null;
 
 $(function() {
-    $("#wb-join-session").click(function() {
-        var url = routePrefix + "/session";
-        $.ajax({
-            url: url,
-            type: "POST",
-            data: "",
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function(data) {
-                alert(data);
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+            width: 100,
+            height: 100
+    });
+    var id = getCookie("wb_session_id");
+
+    // parse id from cookie string
+    function getCookie(cookieName) {
+            var name = cookieName + "=";
+            var ca = document.cookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                    var c = ca[i];
+                    while (c.charAt(0) == ' ') {
+                            c = c.substring(1);
+                    }
+                    if (c.indexOf(name) == 0) {
+                            return c.substring(name.length, c.length);
+                    }
             }
+            return "";
+    }
+
+    qrcode.makeCode(id);
+
+    $("#create_session_btn").click(function() {
+        $.ajax('http://localhost:8080/whiteboarder/session', {
+            type: 'POST',
+            contentType: "application/json",
+            success: function (response) {
+                console.log(response);
+                // make cookie
+                document.cookie = "wb_session_id=" + response + ";path=/";
+            },
+            dataType: 'text'
         });
     });
 });
