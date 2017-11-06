175aca5c5471cfb2f81e01e8f53d4d613ccee221
Jared Gorton <jmgorton@iastate.edu>
2017-11-05 18:22:48 -0600

    fixed syntax error

--- whiteboarder-server/src/main/java/com/model/Edit.java
+++ whiteboarder-server/src/main/java/com/model/Edit.java
@@ -14,7 +14,7 @@ public class Edit {
 	private Set<Point> points;
 	private Date timestamp;
 
-	public Edit(String editID, String wbID, String username, int color, int brushSize, Set<Point> points) {
+	public Edit(int editID, String wbID, String username, int color, int brushSize, Set<Point> points) {
 		this.wbID = wbID;
 		this.editID = editID;
 		this.username = username;
